27227a1b657e03647bc2d34ff97cc73d223ea5e2
Wisdman <wisdman@wisdman.io>
2024-05-24 18:48:49 +0500

    disable debug

--- build.sh
+++ build.sh
@@ -23,7 +23,7 @@ case "$1" in
     ;;
   run)
     echo "---> Running Lit3D-KUKA-C3-Gate"
-    eval 'go run -mod=vendor -tags=dev -ldflags "-X main.version=${VERSION}" ./cmd/kuka-c3-gate -v -d'
+    eval 'go run -mod=vendor -tags=dev -ldflags "-X main.version=${VERSION}" ./cmd/kuka-c3-gate -v'
     ;;
   *)
     echo "Incorrect build target name" >&2
